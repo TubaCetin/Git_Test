9ce5df5299b30a9da05b30655713813e00e2e8e2
TubaCetin <tubac0310@gmail.com>
2020-11-16 10:43:19 +0100

    ikinci guncellemem

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den Merhaba");
         System.out.println("GitHup dan herkese  Merhaba 2. surum guncelleme yapiyorum");
+        System.out.println("intellij den git hup a degisiklik");
+        System.out.println("Hellooooo");
     }
 
 }
